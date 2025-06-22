8a2e726b33de31e609197158c5f7f465a5c3b72a
Luciano <lucianoyague@icloud.com>
2025-06-22 19:27:44 -0300

    se sube archivo .h

---
+++ funciones.h
@@ -0,0 +1,27 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#define MAX_NOMBRE 50
+#define MAX_TELEFONO 20
+#define MAX_EMAIL 50
+
+
+typedef struct {
+    char nombre[MAX_NOMBRE];
+    char telefono[MAX_TELEFONO];
+    char email[MAX_EMAIL];
+} Contacto;
+
+
+typedef struct Nodo {
+    Contacto contacto;
+    struct Nodo* siguiente;
+} Nodo;
+
+
+void cargarAgendaDesdeArchivo(Nodo lista);
+void guardarAgendaEnArchivo(Nodo* lista);
+void agregarContacto(Nodo lista);
+void eliminarContacto(Nodo** lista);
+
+#endif
